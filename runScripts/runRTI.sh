0856a85fe5601d531953962e13c263dcda646fc5
Nicholas Renninger <nire1188@colorado.edu>
2019-09-05 00:34:27 -0600

    adding scripts folder to interact with carfollowingRTI, with script to run RTI binary

---
+++ runScripts/runRTI.sh
@@ -0,0 +1,44 @@
+#!/bin/zsh
+
+# relative paths are based on this directory structure:
+# .
+# └── carFollowingRTISandbox
+#     ├── carfollowingRTI
+#     │   ├── MOHA
+#     │   │   ├── data
+#     │   │   ├── ori
+#     │   │   ├── parameters
+#     │   │   ├── sub_function
+#     │   │   └── train_cluster
+#     │   ├── MatlabCode_IFAC
+#     │   │   ├── results
+#     │   │   ├── test
+#     │   │   ├── test_Higgs
+#     │   │   ├── test_sym
+#     │   │   ├── train
+#     │   │   ├── train_Higgs
+#     │   │   └── train_sym
+#     │   ├── data
+#     │   ├── rti
+#     │   │   └── build
+#     │   └── video
+#     ├── conda
+#     └── runScripts
+#         ├── runRTI.sh
+#         │
+#         ...
+
+CARFOLLOWINGRTISANDBOX_PATH="$HOME/Google Drive/Grad School/specification learning/code/carFollowingRTISandbox/carfollowingRTI"
+
+# specifying input file to feed to rti
+INPUT_FILE="$CARFOLLOWINGRTISANDBOX_PATH/MOHA/data/I80_1/rti_train_I80_1.txt"
+
+# from RTI Docs:
+# 
+# 1 specifies the used method (1 for likelihood ratio, 2 for chi-squared)
+# 0.05 is the significance level used in the tests
+# filename is a file in the following format:
+
+# int int                                     (number_of_strings size_of_alphabet)
+# int char int char int char int ... char int (length_of_string symbol1 time_delay1 s2 t2 .. sn tn)
+$CARFOLLOWINGRTISANDBOX_PATH/rti/build/rti 1 0.05 $INPUT_FILE
